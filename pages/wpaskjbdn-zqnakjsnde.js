5874868b3cb5688963b8fe0ec57cf53ca57edc1f
Vibush Shanmugam <vibushks@tamu.edu>
2023-03-29 12:45:44 -0500

    Initial Commit

---
+++ pages/wpaskjbdn-zqnakjsnde.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>wpaskjbdn zqnakjsnde</title>
+        <meta name="description" content="wpaskjbdn zqnakjsnde"/>
+        <meta name="robots" content="follow, index" />
+      </Head>
+      <h1>First Post</h1>
+      <p>This is a webpage designed for the CSCE670 Information Storage and Retrieval course at TAMU.</p>
+      <p>This page is designed for the search query: <b>wpaskjbdn zqnakjsnde</b></p>
+    </Layout>
+  );
+}
