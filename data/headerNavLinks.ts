164daf4ffa18377984bfafefdfa2671eb25ec7e6
Vibush Shanmugam <vibushks@tamu.edu>
2023-03-30 11:29:26 -0500

    Revert back to blog instead of articles

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/wpaskjbdn-zqnakjsnde', title: 'About' },
-  { href: '/articles', title: 'Articles' },
+  { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
 ]
