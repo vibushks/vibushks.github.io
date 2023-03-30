9162f09826b85ea88cdcaf6efd4649de9a301b35
Vibush Shanmugam <vibushks@tamu.edu>
2023-03-29 19:20:07 -0500

    Update website base url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,8 +9,8 @@ const siteMetadata = {
   googleSiteVerification: 'njgp4_FceX1LPzcils-uOG2J48pmCSvc6lEl5aT33Ew',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.vibush-shanmugam.com/',
+  siteRepo: 'https://github.com/vibushks/vibushks.github.io',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
