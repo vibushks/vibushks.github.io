61f89af9c7884b9408eb85bb97bc27eca14d9281
Vibush Shanmugam <vibushks@tamu.edu>
2023-03-30 10:49:51 -0500

    Deploy to Github Pages

---
+++ _next/static/lokJvjLXQylaIes4i6evK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[...slug]","\u002Fblog\u002Fpage\u002F[page]","\u002Ftags","\u002Ftags\u002F[tag]","\u002Fwpaskjbdn-zqnakjsnde"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
