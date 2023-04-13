34642b18b81392105b33ddb7f95253330710afbe
Vibush Shanmugam <vibushks@tamu.edu>
2023-04-13 16:54:21 -0500

    Update blog content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,12 +7,24 @@ const projectsData = [
     href: 'https://en.wikipedia.org/wiki/List_of_search_engines',
   },
   {
+    title: 'Drake Bell Found Safe, Police Confirm',
+    description: `At 1:26 p.m. ET on Thursday, actor Drake Bell was confirmed to be “safe” and in contact with law enforcement officials, the Daytona Beach Police Department reported.`,
+    imgSrc: '/static/images/drake-bell.jpg',
+    href: 'https://variety.com/2023/tv/news/drake-bell-missing-nickelodeon-police-daytona-1235582487/',
+  },
+  {
     title: 'The Time Machine',
     description: `Imagine being able to travel back in time or to the future to see wpaskjbdn zqnakjsnde. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
+  },
+  {
+    title: 'French protesters storm headquarters of luxury giant LVMH',
+    description: `Hundreds of thousands of people took part in a fresh round of demonstrations across France on Thursday over government plans to raise the retirement age from 62 to 64, a day before a crucial court ruling on the constitutionality of the divisive law.`,
+    imgSrc: '/static/images/french-protest.jpg',
+    href: 'https://www.cnn.com/2023/04/13/europe/france-protests-pensions-lvmh-intl/index.html',
   },
 ]
 
